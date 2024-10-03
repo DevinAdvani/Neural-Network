7cef27115e194c0934966fee681a750dfa61b140
DevinAdvani <advani.devin@gmail.com>
2024-10-03 16:11:49 +0100

    Can inspect data set now

--- main.py
+++ main.py
@@ -41,39 +41,48 @@ while True:
     print("Type 'A' for adding to the dataset")
     print("Type 'I' for inspecting the dataset")
     print("Type 'E' for exiting")
-
     choice = input(":")
     if choice == "A":
         character = input("WHICH CHARACTER: ")
     if choice == "E":
         break
 
-    size = 100
-    line_id = None
-    line_points = []
-    line_options = {}
-    drawing = []
-    root = tk.Tk()
-    canvas = tk.Canvas(width=size, height=size)
-    canvas.pack()
-    canvas.bind('<Button-1>', set_start)
-    canvas.bind('<B1-Motion>', draw_line)
-    canvas.bind('<ButtonRelease-1>', end_line)
-    root.mainloop()
+    if choice == "T" or choice == "A":
+        size = 100
+        line_id = None
+        line_points = []
+        line_options = {}
+        drawing = []
+        root = tk.Tk()
+        canvas = tk.Canvas(width=size, height=size)
+        canvas.pack()
+        canvas.bind('<Button-1>', set_start)
+        canvas.bind('<B1-Motion>', draw_line)
+        canvas.bind('<ButtonRelease-1>', end_line)
+        root.mainloop()
 
-    matrix = []
-    for i in range(0,size):
-        row = []
-        for j in range(0,size):
-            row.append(0)
-        matrix.append(row)
+        matrix = []
+        for i in range(0,size):
+            row = []
+            for j in range(0,size):
+                row.append(0)
+            matrix.append(row)
 
-    for i in range(0,len(drawing)):
-        try:
-            matrix[drawing[i][1]][drawing[i][0]] = 1
-        except:
-            pass
+        for i in range(0,len(drawing)):
+            try:
+                matrix[drawing[i][1]][drawing[i][0]] = 1
+            except:
+                pass
+    
+    if choice == "I":
+        f = open("data_set.txt", "r")
+        for x in f:
+            print(x[2])
+            for i in range(0,100):
+                print(x[7 + 100 * i: 107 + 100 * i])
+            
     
     if choice == "A":
         print("Thank you for the input")
+        print("")
         store_matrix(matrix, character)
